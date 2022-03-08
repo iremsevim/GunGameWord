1b150890490bd8578e578b8ad46817377d0cc2cb
Mustafa Coşkuner <coskunersoft@outlook.com>
2022-03-08 12:07:40 +0300

    Update

--- Assets/Scripts/GameGeneral/GameData.cs
+++ Assets/Scripts/GameGeneral/GameData.cs
@@ -5,4 +5,12 @@ using Coskunerov.Actors;
 public class GameData : GameSingleActor<GameData>
 {
     public GameObject player;
+    public List<LetterProfile> allLetters;
+      
+    [System.Serializable]
+    public class LetterProfile
+    {
+
+        public Sprite letter;
+    }
 }
